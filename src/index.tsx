8775b01d1cf4a9d9052f9409dd7aa53eefa9432f
Selahattin Demir <selahattindemir686@gmail.com>
2023-01-11 20:47:38 +0300

    wrapped providers  in App folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,26 @@
+/* eslint-disable @typescript-eslint/no-redeclare */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './redux';
 import './index.css';
+import './custom.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from './ThemeContext/ThemeProvider';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </ThemeProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
