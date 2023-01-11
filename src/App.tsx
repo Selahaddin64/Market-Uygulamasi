75ee69bcad5c2f21a6b1804cf07e4b4fe6bd3d2b
Selahattin Demir <selahattindemir686@gmail.com>
2023-01-11 15:01:23 +0300

    added theme option

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
-import Cart from './pages/Cart';
-import Favorites from './pages/Favorites';
-import Products from './pages/Products';
+import Cart from './pages/Cart/index';
+import Favorites from './pages/Favorites/index';
+import Products from './pages/Products/index';
+import { useThemeHook } from './ThemeContext/ThemeProvider';
 
-function App() {
+const App = () => {
+  const [theme] = useThemeHook();
   return (
-    <BrowserRouter>
-    <Header />
-      <Routes>
-        <Route index element={<Products/>} />
-        <Route path="product" element={<Products/>} />
-        <Route path="cart" element={<Cart />} />
-        <Route path="favorites" element={<Favorites />} />
-      </Routes>
-    </BrowserRouter>
+    <main className={theme ? 'bg-black' : 'bg-light-2'} style={{ height: '100vh', overflowY: 'auto' }}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route index element={<Products />} />
+          <Route path='product' element={<Products />} />
+          <Route path='cart' element={<Cart />} />
+          <Route path='favorites' element={<Favorites />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
   );
-}
-
+};
 export default App;
