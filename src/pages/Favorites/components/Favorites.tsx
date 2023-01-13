71f4a985318d42cd3fc3b4092e76760eecf16688
Selahattin Demir <selahattindemir686@gmail.com>
2023-01-14 00:09:05 +0300

    added theme option

--- src/pages/Favorites/components/Favorites.tsx
+++ src/pages/Favorites/components/Favorites.tsx
@@ -6,15 +6,17 @@ import { FavProducts } from '../../../redux/Favorites/favorites.types';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../redux/Language/store';
 import { translate } from '../../../i18n';
+import { useThemeHook } from '../../../ThemeContext/ThemeProvider';
 
-interface ICartProps {
-  setIsSubmitOrder: (isSubmitOrder: boolean) => void;
+interface IFavoritesProps {
   products: FavProducts[];
 }
 
-const Favorites: FC<ICartProps> = ({ products }) => {
+const Favorites: FC<IFavoritesProps> = ({ products }) => {
   const { language } = useSelector((state: RootState) => state.lang);
-  if (!products.length) return <p>{translate('Favorites is empty', language)}</p>;
+  const [theme] = useThemeHook();
+
+  if (!products.length) return <h3 className={`${theme ? 'text-light my-5' : 'text-black my-5'} pt-16 text-center`}>{translate('Favorites is empty', language)}</h3>;
 
   return (
     <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
